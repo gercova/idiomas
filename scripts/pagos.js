f346103bedf0200ea9367144b3eec74b5720e8c4
German Cotrina Valles <64036205+gercova@users.noreply.github.com>
2021-08-18 18:02:34 -0500

    german script
    listar pagos pendientes

--- scripts/pagos.js
+++ scripts/pagos.js
@@ -10,81 +10,81 @@ $(document).ready(function(){
         $('#estudiante').val(infogrupo[4]);
         $('#curso').val(infogrupo[5]);
         $('#deuda').val(infogrupo[6]);
+        $('#id_curso').val(infogrupo[7]);
         $('#modal-prematricula').modal('hide');
     });
-    // TABLA PAGOS
-    $('.btn-add-info-payment').on('click', function(){
-        html = '<tr>';
-        html += '<td><input type="number" class="form-control" name="codigo[]" required></td>';
-        html += '<td><input type="number" class="form-control" name="monto[]" required></td>';
-        html += '<td><select class="form-control" name="detalle[]" required><option value="">-- Seleccione --</option><option value="MATRICULA">Matrícula</option><option value="PENSION">Pensión de enseñanza</option><option value="OTROS">Otros</option></select></td>';
-        html += '<td><input type="date" class="form-control" name="fecha[]" required></td>';
-        html += '<td><button type="button" class="btn btn-danger btn-remove-info"><span class="fa fa-remove"></span></button></td>';
-        html += '</tr>';
-        $('#tb-payment tbody').append(html);
-    });
-    // remover modulos filas de la tabla
-    $(document).on('click','.btn-remove-info', function(){
-        $(this).closest('tr').remove();
-    });
 });
+
 function addRow(){
-    $("#addRowBtn").button("loading");
-    var tableLength = $("#productTable tbody tr").length;
-    var tableRow;
-    var arrayNumber;
-    var count;
-    if(tableLength > 0){		
-        tableRow = $("#productTable tbody tr:last").attr('id');
-        arrayNumber = $("#productTable tbody tr:last").attr('class');
-        count = tableRow.substring(3);	
-        count = Number(count) + 1;
-        arrayNumber = Number(arrayNumber) + 1;					
+	//$("#addRowBtn").button("loading");
+    id_curso = $('#id_curso').val();
+    if(id_curso != ''){
+        //alert($id_curso);
+        var tableLength = $("#table-payment tbody tr").length;
+        var tableRow;
+        var arrayNumber;
+        var count;
+        if(tableLength > 0){		
+            tableRow    = $("#table-payment tbody tr:last").attr('id');
+            arrayNumber = $("#table-payment tbody tr:last").attr('class');
+            count       = tableRow.substring(3);	
+            count       = Number(count) + 1;
+            arrayNumber = Number(arrayNumber) + 1;					
+        }else{
+            // no table row
+            count = 1;
+            arrayNumber = 0;
+        }
+        $.ajax({
+            url: base_url + 'movimientos/pagos/getCostoCurso',
+            type: 'post',
+            dataType: 'json',
+            data: {id_curso: id_curso},
+            success:function(response){
+                //$("#addRowBtn").button("reset");
+                //console.log(response);			
+                var tr = '<tr id="row'+count+'" class="'+arrayNumber+'">'+			  				
+                    '<td>'+
+                        '<select class="form-control" name="concepto[]" id="concepto'+count+'" required>'+
+                            '<option value="">-- Seleccione concepto --</option>';
+                            $.each(response, function(index, value) {
+                                //console.log(value.id);
+                                tr += '<option value="'+value.id+'">'+value.descripcion+' S/.'+value.costo+'</option>';				
+                            });						
+                        tr += '</select>'+
+                    '</td>'+
+                    '<td>'+
+                        '<input type="number" name="vaucher[]" id="vaucher'+count+'" autocomplete="off" class="form-control" required>'+
+                    '</td>'+
+                    '<td>'+
+                        '<input type="text" name="descripcion[]" id="descripcion'+count+'" autocomplete="off" class="form-control" required>'+
+                    '</td>'+
+                    '<td>'+
+                        '<input type="number" name="monto[]" id="monto'+count+'" autocomplete="off" class="form-control" required>'+
+                    '</td>'+
+                    '<td>'+
+                        '<input type="date" name="fecha[]" id="fecha'+count+'" autocomplete="off" class="form-control" required>'+
+                    '</td>'+
+                    '<td>'+
+                        '<button class="btn btn-default removeProductRowBtn" type="button" onclick="removeProductRow('+count+')"><i class="glyphicon glyphicon-trash"></i></button>'+
+                    '</td>'+
+                '</tr>';
+                if(tableLength > 0){							
+                    $("#table-payment tbody tr:last").after(tr);
+                }else{				
+                    $("#table-payment tbody").append(tr);
+                }		
+            } // /success
+        });	// get the product data  
     }else{
-        // no table row
-        count = 1;
-        arrayNumber = 0;
+        alert('Seleccione alumno');
     }
-    $.ajax({
-        url: 'php_action/fetchProductData.php',
-        type: 'post',
-        dataType: 'json',
-        success:function(response){
-            $("#addRowBtn").button("reset");			
-            var tr = '<tr id="row'+count+'" class="'+arrayNumber+'">'+			  				
-                '<td>'+
-                    '<div class="form-group">'+
-                    '<select class="form-control" name="productName[]" id="productName'+count+'" onchange="getProductData('+count+')" >'+
-                        '<option value="">~~SELECT~~</option>';
-                        // console.log(response);
-                        $.each(response, function(index, value) {
-                            tr += '<option value="'+value[0]+'">'+value[1]+'</option>';							
-                        });						
-                    tr += '</select>'+
-                    '</div>'+
-                '</td>'+
-                '<td style="padding-left:20px;"">'+
-                    '<input type="text" name="rate[]" id="rate'+count+'" autocomplete="off" disabled="true" class="form-control" />'+
-                    '<input type="hidden" name="rateValue[]" id="rateValue'+count+'" autocomplete="off" class="form-control" />'+
-                '</td style="padding-left:20px;">'+
-                '<td style="padding-left:20px;">'+
-                    '<div class="form-group">'+
-                    '<input type="number" name="quantity[]" id="quantity'+count+'" onkeyup="getTotal('+count+')" autocomplete="off" class="form-control" min="1" />'+
-                    '</div>'+
-                '</td>'+
-                '<td style="padding-left:20px;">'+
-                    '<input type="text" name="total[]" id="total'+count+'" autocomplete="off" class="form-control" disabled="true" />'+
-                    '<input type="hidden" name="totalValue[]" id="totalValue'+count+'" autocomplete="off" class="form-control" />'+
-                '</td>'+
-                '<td>'+
-                    '<button class="btn btn-default removeProductRowBtn" type="button" onclick="removeProductRow('+count+')"><i class="glyphicon glyphicon-trash"></i></button>'+
-                '</td>'+
-            '</tr>';
-            if(tableLength > 0){							
-                $("#productTable tbody tr:last").after(tr);
-            }else{				
-                $("#productTable tbody").append(tr);
-            }		
-        } // /success
-    });	// get the product data
 } // /add row
+
+function removeProductRow(row = null) {
+	if(row) {
+		$("#row"+row).remove();
+	}else{
+		alert('Algo salió mal, recargue la página');
+	}
+}
